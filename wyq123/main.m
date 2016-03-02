06b4f67f9ca9c5934a81a5e292834e54288a9ee4
qianfeng <m13121291992@163.com>
2016-03-02 13:18:57 +0800

    Initial Commit

---
+++ wyq123/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wyq123
+//
+//  Created by qianfeng on 16/3/2.
+//  Copyright © 2016年 武跃强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
